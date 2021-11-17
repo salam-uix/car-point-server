9c8a746aaa7409a24f25a04c5d7817f090e7f6fd
Abdus salam <salam.ads97@gmail.com>
2021-11-18 03:09:32 +0600

    z

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 
 
@@ -10,17 +11,56 @@ const port = process.env.PORT || 5000;
 
 //middle wire
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiptd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri)
 
 async function run() {
     try {
         await client.connect();
-        console.log('database connected sucesfully')
+        const database = client.db('carPoint');
+        const productsCollection = database.collection('products');
+
+        //GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({})
+            const products = await cursor.toArray();
+            res.send(products)
+        });
+
+        //GET Single Products
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific products', id);
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+        })
+
+        //POST API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log('Hit the api', product)
+
+            const result = await productsCollection.insertOne(product);
+            console.log(result);
+            res.json(result);
+        });
+
+        //DELETE API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result)
+
+        })
+
+
+
     }
     finally {
         // await client.close();
@@ -31,7 +71,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Doctors portal!')
+    res.send('Hello car points!')
 })
 
 app.listen(port, () => {
